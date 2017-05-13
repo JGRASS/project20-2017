1a5e623130d4103692736764578033adfef3ad3e
AnaColovic <ana.colovic996@gmail.com>
2017-05-13 16:13:55 +0200

    Ispravljena greska

--- FilmoviPreporuka/src/pretrazivac/Korisnik.java
+++ FilmoviPreporuka/src/pretrazivac/Korisnik.java
@@ -125,8 +125,9 @@ public class Korisnik implements KorisnikInterfejs {
 	 * Metoda postavlja sifru korisnika na unetu vrednost
 	 * @param pass predstavlja sifru korisnika kao String
 	 * @throws RuntimeException
-	 * <ul> kada je uneta sifra null vrednost
-	 * <ul> kada je sifra kraca od 8 znakova
+	 * <ul>
+	 * <li> kada je uneta sifra null vrednost
+	 * <li> kada je sifra kraca od 8 znakova
 	 * </ul>
 	 */
 	public void setPass(String pass) throws RuntimeException{
